8d4ad1c014d115bdf065ba3e9a936eebc8d997c5
ZAAL <pk.mixer@gmail.com>
2020-12-03 16:09:51 +0300

    Added 2nd lesson checks and 1 more operation.

--- 2nd lesson/main.go
+++ 2nd lesson/main.go
@@ -3,22 +3,39 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	var a, b, res float32
+	var c, d int
 	var op string
+	var err error
 
 	fmt.Print("Добро пожаловать в калькулятор :) \n")
 	fmt.Print("Введите первое число: ")
-	fmt.Scanln(&a)
-	fmt.Println(os.Stderr)
+	_, err = fmt.Scanln(&a)
+	if err != nil {
+		fmt.Printf("введено не число")
+		os.Exit(0)
+	}
+
+	//fmt.Println(os.Stderr)
 
 	fmt.Print("Введите второе число: ")
-	fmt.Scanln(&b)
+	_, err = fmt.Scanln(&b)
+	if err != nil {
+		fmt.Printf("введено не число")
+		os.Exit(0)
+	}
 
-	fmt.Print("Введите арифметическую операцию (+, -, *, /): ")
+	fmt.Print("Введите арифметическую операцию (+, -, *, /,%-взятие остатка.): ")
 	fmt.Scanln(&op)
+
+	if op == "%" {
+		c = int(a)
+		d = int(b)
+	}
 
 	switch op {
 	case "+":
@@ -29,9 +46,14 @@ func main() {
 		res = a * b
 	case "/":
 		res = a / b
+	case "%":
+		fmt.Printf("Остаток от деления: %s", strconv.Itoa(c%d))
+		break
 	default:
 		fmt.Println("Операция выбрана неверно")
 		os.Exit(1)
 	}
-	fmt.Printf("Результат выполнения операции: %f\n", res)
+	if !(op == "%") {
+		fmt.Printf("Результат выполнения операции: %f\n", res)
+	}
 }

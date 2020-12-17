1fb2452705eff2f4f0f94b93df0b6dcba40a9ceb
ZAAL <pk.mixer@gmail.com>
2020-12-18 00:01:32 +0300

    Добавлен вариант решения с рекурсией.

--- 3rd_lesson/main.go
+++ 3rd_lesson/main.go
@@ -5,7 +5,6 @@ import (
 	"os"
 )
 
-//Fibonacci recursive
 func fib(n int) int {
 	fn := make(map[int]int)
 	for i := 0; i <= n; i++ {
@@ -20,6 +19,25 @@ func fib(n int) int {
 	return fn[n]
 }
 
+func fibr(i int, fn map[int]int) int {
+	var f int
+
+	if i <= 2 {
+		fn[i] = 1
+		f = 1
+	} else {
+		//Проверяем если уже есть рассчитанный элемент при обращении в рекурсию
+		//выдаем существующий элемент, а не считаем заново.
+		if len(fn) == i {
+			//fmt.Printf("\nПошла рекурсия для %d\n", len(fn))
+			f = fibr(i-2, fn) + fibr(i-1, fn)
+		} else {
+			f = fn[i-2] + fn[i-1]
+		}
+	}
+	return f
+}
+
 func main() {
 	var a int
 	var err error
@@ -32,4 +50,14 @@ func main() {
 	for i := 0; i < a; i++ {
 		fmt.Printf("%d, ", fib(i))
 	}
+	//Поиск фибо по рекурссии
+	fn := make(map[int]int)
+	fmt.Printf("\nФибо по рекурсии:\n")
+	for j := 0; j < a; j++ {
+		f := fibr(j, fn)
+		fn[j] = f
+		fmt.Printf("%d, ", f)
+
+	}
+
 }
